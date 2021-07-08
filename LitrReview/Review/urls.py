0ab103b7c4a3227cdc568ce323655914c67c9b81
Corentin BRAVO <corentinbravo@gmail.com>
2021-07-08 10:25:40 +0200

    Some placeholders URL + all URLS to create ticket/reviews

---
+++ LitrReview/Review/urls.py
@@ -0,0 +1,27 @@
+"""LitrReview URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register', views.register, name='register'),
+    path('register', views.register, name='feed'),
+    path('register', views.register, name='posts'),
+    path('register', views.register, name='follows'),
+    path('ticket-creation/', views.TicketCreate.as_view(), name='ticket-create'),
+    path('review-creation/', views.ReviewAndTicketCreate.as_view(), name='review-create'),
+    path('review-creation/<int:pk>', views.ReviewCreate.as_view(), name='review-create-with-ticket'),
+]
