f05cf2952e4ae564aaa3a3f2aff98a778d3c2c35
Corentin BRAVO <corentinbravo@gmail.com>
2021-07-20 18:35:07 +0200

    Removed help_texts. Turned the rating display for reviews in a radio select.

--- LitrReview/Review/forms.py
+++ LitrReview/Review/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.db import models
-from django.forms.widgets import PasswordInput
+from django.forms.widgets import PasswordInput, RadioSelect
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError, ObjectDoesNotExist
 
@@ -10,18 +10,18 @@ from .models import Ticket, Review, UserFollows
 class RegisterForm(forms.Form):
     user_name = forms.CharField(min_length=5,
                                 max_length=32,
-                                label="Nom d'utilisateur",
-                                help_text="Entrez le nom qui vous servira à vous connecter, sa longueur doit être "
-                                          "comprise entre 5 et 32 caractères")
+                                label="Nom d'utilisateur")
+    # help_text="Entrez le nom qui vous servira à vous connecter, sa longueur doit être comprise entre
+    # 5 et 32 caractères"
     password = forms.CharField(min_length=8,
                                label="Mot de passe",
-                               help_text="Entrez le mot de passe que vous utiliserez pour vous connecter. Sa "
-                                         "longueur doit être supérieure à 8 caractères.",
                                widget=PasswordInput)
+    # help_text="Entrez le mot de passe que vous utiliserez pour vous connecter.
+    # Sa longueur doit être supérieure à 8 caractères.",
     password_check = forms.CharField(min_length=8,
                                      label="Confirmer mot de passe",
-                                     help_text="Réentrez votre mot de passe pour le confirmer.",
                                      widget=PasswordInput)
+    #  help_text="Réentrez votre mot de passe pour le confirmer."
 
     def clean(self):
         data = super().clean()
@@ -49,6 +49,10 @@ class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
         fields = ["title", "description", "image"]
+        labels = {"title": "Titre"}
+        # help_texts = {"title": "Entrez le titre du livre et/ou son auteur",
+        #               "description": "Donnez les détails de ce que vous cherchez.",
+        #               "image": "Téléversez la couverture du livre (facultatif)"}
 
 
 class ReviewForm(forms.ModelForm):
@@ -56,14 +60,22 @@ class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ["headline", "rating", "body"]
+        labels = {"headline": "Titre",
+                  "rating": "Note",
+                  "body": "Commentaire"}
+        widgets = {"rating": RadioSelect(choices=[(0, "- 0"),
+                                                  (1, "- 1"),
+                                                  (2, "- 2"),
+                                                  (3, "- 3"),
+                                                  (4, "- 4"),
+                                                  (5, "- 5")],
+                                         attrs={"class": "list-unstyled d-flex flex-row justify-content-around"})}
 
 
 class FollowForm(forms.ModelForm):
     followed_user = forms.CharField(min_length=5,
                                     max_length=32,
-                                    label="Nom d'utilisateur",
-                                    help_text="Entrez le nom qui vous servira à vous connecter, sa longueur doit être "
-                                              "comprise entre 5 et 32 caractères")
+                                    label="Nom d'utilisateur")
 
     class Meta:
         model = UserFollows
