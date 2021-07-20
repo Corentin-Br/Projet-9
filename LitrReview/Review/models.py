875f0f1a78f985e4564343f311e9032e8e72073b
Corentin BRAVO <corentinbravo@gmail.com>
2021-07-20 18:35:16 +0200

    Removed help_texts.

--- LitrReview/Review/models.py
+++ LitrReview/Review/models.py
@@ -4,10 +4,10 @@ from django.db import models
 
 
 class Ticket(models.Model):
-    title = models.CharField(max_length=128, verbose_name="Titre", help_text="Entrez le titre du et l'auteur du livre.")
-    description = models.TextField(max_length=2048, blank=True, help_text="Donnez les détails de ce que vous cherchez.")
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=2048, blank=True)
     user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    image = models.ImageField(null=True, blank=True, help_text="Téléversez la couverture du livre (facultatif)")
+    image = models.ImageField(null=True, blank=True)
     time_created = models.DateTimeField(auto_now_add=True)
 
 
@@ -15,7 +15,7 @@ class Review(models.Model):
     ticket = models.OneToOneField(to=Ticket, on_delete=models.CASCADE)
     rating = models.PositiveSmallIntegerField(
         # validates that rating must be between 0 and 5
-        validators=[MinValueValidator(0), MaxValueValidator(5)])
+        validators=[MinValueValidator(0), MaxValueValidator(5)],)
     headline = models.CharField(max_length=128)
     body = models.TextField(max_length=8192, blank=True)
     user = models.ForeignKey(
